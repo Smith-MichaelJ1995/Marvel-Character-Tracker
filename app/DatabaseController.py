e2844ac1fd3ea599244a5b04bcc1d6c8a5579d3b
Michael Smith <michaeljoshuasmith1@gmail.com>
2022-06-03 17:34:18 -0400

    Able to insert records with more speed, programmatic efficiency and can include comics information. Now need to update view cache with newly inserted records.

---
+++ app/DatabaseController.py
@@ -0,0 +1,145 @@
+import mysql.connector
+from mysql.connector import errorcode
+import os
+import json
+
+# instantiate database controller
+class DatabaseController:
+
+    # perform connection operation
+    def connect(self):
+        try:
+            return mysql.connector.connect(
+                host=os.environ['SQL_HOST'],
+                user=os.environ['SQL_USER'],
+                password=os.environ['SQL_PASSWORD'],  
+                database=os.environ['SQL_DATABASE'],               
+                port=os.environ['SQL_PORT'],
+            )
+
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+            exit()
+
+    # perform operation on class instantiation
+    def populate(self):
+
+        # create temporary records placeholder
+        records = {}
+
+        # fetch all tables in "intel" database
+        self.myCursor.execute("show TABLES;")
+        tables = self.myCursor.fetchall()
+
+        # traverse through all tables
+        for table in tables:
+
+            # extract column name
+            tableName = table[0]
+            searchResults = []
+
+            # extract all records from table in question
+            self.myCursor.execute("SELECT * from {}".format(tableName))
+            rows = self.myCursor.fetchall()
+
+            # fetch all records from all rows
+            for index, row in enumerate(rows):
+
+                # extract key/value fields
+                heroNumber = row[0]
+                heroName = row[1]
+                heroComics = row[2]
+                heroThumbnail = row[3]
+                heroDescription = row[4]
+
+                # append search results for future processing
+                searchResults.append({
+                    "id": heroNumber,
+                    "name": heroName,
+                    "comics": heroComics,
+                    "img": heroThumbnail,
+                    "description": heroDescription 
+                })
+            
+            
+            # append item to records container
+            records[tableName] = searchResults
+            
+        return records
+
+    # given string, remove bad sql characters
+    def remove_bad_characters(self, inpt):
+        for badChar in self.badChars:
+            inpt = inpt.replace(badChar, "")
+        return inpt
+        
+    # return all table names within intel database
+    def return_table_names(self):
+        return self.records.keys()
+
+    # return table records to viewer
+    def return_records_from_table(self, tableName):
+        if tableName in self.records.keys():
+            return self.records[tableName]
+        else:
+            return []
+        
+
+    # instantiate expected variables
+    def __init__(self):
+
+        # generate DB connection object
+        self.myDB = self.connect()
+
+        # generate cursor for inline command invocation
+        self.myCursor = self.myDB.cursor()
+
+        # declare list of known bad characters for safety
+        self.badChars = ['&','<','>','/','\\','"',"'",'?','+']
+
+        # use selected database
+        self.myCursor.execute("USE intel;")
+
+        # populate records from database
+        self.records = self.populate()
+
+    # insert records into databases specified table
+    def insert_records(self, tableName, tableRecords):  
+
+        # given list of dictionary records, generate list of tuples ready to insert
+        # def generate_tuple_of_data(): 
+        #     tableRecordsSet = []
+        #     for record in tableRecords.values():
+        #         # print(json.dumps(record, indent=4))
+        #         # exit()
+        #         tableRecordsSet.append(set(record['id'], record['name'], record['comics'], record['img'], record['description']))
+        #     return tableRecordsSet
+
+        # insert into self.records
+        # self.records[tableName] = tableRecords.value
+
+        # print("tableRecords = {}".format(self.records[tableName]))
+        # print("\n\n\n")
+        # print(tableRecords)
+        # exit()
+
+        
+        # create table
+        cmd = """CREATE TABLE {} (id INTEGER, name VARCHAR(255), comics VARCHAR(1000), image VARCHAR(255), description VARCHAR(1000));""".format(tableName)
+        #cmd = """CREATE TABLE spectrum (id INTEGER, name VARCHAR(255), image VARCHAR(255), description VARCHAR(1000));"""
+        self.myCursor.execute(cmd)
+
+        # insert records into table
+        # DEFINE INSERTION STATEMENT, PROTECT AGAINST SQL INJECTION
+        sqlStatement = """INSERT INTO {} (id, name, comics, image, description) VALUES (%s, %s, %s, %s, %s)""".format(tableName)
+        
+        # invoke statement, add records.
+        self.myCursor.executemany(sqlStatement, tableRecords)
+
+        # changes must be commited to the database in order to take effect
+        self.myDB.commit()
---
+++ app/MarvelController.py
@@ -0,0 +1,162 @@
+from datetime import datetime
+import os
+import hashlib
+import requests
+import json
+
+# integrate Database class controller
+from DatabaseController import DatabaseController
+
+class MarvelController():
+
+    # generate API auth credentials
+    def fetch_config(self):
+
+        # GENERATE TIMESTAMP
+        #ts = int(round(datetime.now().timestamp()))
+
+        # GENERATE PRE-HASH-STRING
+        preHashContents = self.TIMESTAMP + self.SECRET + self.KEY 
+
+        # perform URL encoding
+        hsh = hashlib.md5(preHashContents.encode('utf-8')).hexdigest()
+
+        # provide query config contents to caller
+        return { "timestamp": self.TIMESTAMP, "key": self.KEY, "hash": hsh }
+
+
+    # instantiate API key-secret variables
+    def __init__(self, databaseController):
+        self.KEY = os.environ.get('MARVEL_API_KEY')
+        self.SECRET = os.environ.get("MARVEL_API_SECRET")
+        self.TIMESTAMP = os.environ.get("MARVEL_API_TIMESTAMP")
+        self.config = self.fetch_config()
+        self.base_url = "https://gateway.marvel.com:443/v1/public/"
+        self.credentials_stamp = "ts={}&apikey={}&hash={}".format(
+            self.config['timestamp'],
+            self.config['key'], 
+            self.config['hash']
+        )
+        self.characters = None
+        self.dbController = databaseController
+
+    # # return character profile based on name
+    def fetch_content_from_api(self, queryURI):
+        
+        # invoke marvel API
+        try:
+
+            # build URL request method
+            url = self.base_url + queryURI + self.credentials_stamp
+
+            # WORKS
+            #url = "https://gateway.marvel.com:443/v1/public/characters?name=Morbius&ts=1654164246&apikey=2a346d86972cc65d7982367dae7758af&hash=c74292bea3e7719c8c7f553435a88f67"
+
+            # DOESNT WORK
+            #url = "https://gateway.marvel.com:443/v1/public/characters?name='Morbius'&ts=1654164802&apikey=2a346d86972cc65d7982367dae7758af&hash=c1eb37eddc89b622161a8aa368d12057"
+
+            #print("ATTEMPTING: {}".format(url))
+
+            # generate response body
+            res = requests.get(url, verify=False).json()
+
+            #https://gateway.marvel.com:443/v1/public/characters?name=ace&ts=1654136081&apikey=2a346d86972cc65d7982367dae7758af&hash=0a6c68ce57dcb731dbb49be859124363
+
+            # perform HTTP web request
+            return res
+
+
+        except Exception as e:
+
+            # display error content to screen
+            print("The Following Exception Occured: {}".format(e))
+
+            # return empty object to user
+            return {}
+    
+    # FIX RECORDS SO THEY RETURN BACK NEATLY FORMATTED DATA READY FOR DB CONTROLLER
+    # given characterName, query information from API
+    def fetch_target_character_from_api(self, characterName):
+
+        # query API for character record
+        rawCharacterRecord = self.fetch_content_from_api(queryURI="characters?name={}&".format(characterName))['data']['results']
+
+        # create placeholder for finalCharacterResultsList
+        finalCharactersResultsList = {}
+
+        # determine if null record
+        if len(rawCharacterRecord) == 0:
+            return ()
+        else:
+
+            # extract raw character record for caller
+            rawCharacterRecord = rawCharacterRecord[0]
+
+            # create placeholder for characters from other comics
+            # instantiating list with target character
+            finalCharactersResultsList[rawCharacterRecord['id']] = (
+                rawCharacterRecord['id'],
+                rawCharacterRecord['name'],
+                self.fetch_comics_from_character_record(rawCharacterRecord['comics']['items']),
+                rawCharacterRecord['thumbnail']['path'] + "." + rawCharacterRecord['thumbnail']['extension'],
+                rawCharacterRecord['description']
+            )
+
+
+            # GRAB CHARACTERS FROM ALL RELATED COMICS
+            for comic in rawCharacterRecord["comics"]["items"]:
+
+                # extract comicId from comic['resourceURI']
+                comicId = comic['resourceURI'].split("/")[-1]
+
+                # fetch all characters from target comic
+                finalCharactersResultsList = self.fetch_characters_from_target_comic(
+                    comicId=comicId,
+                    finalCharactersResultsList=finalCharactersResultsList
+                )
+
+                break
+
+            # provide neatened characters list to caller
+            return finalCharactersResultsList
+                # save character object for later processing
+                # finalCharactersResultsList.extend(
+                #     charactersFromTargetComic
+                # )
+
+            # remove duplicate characters found from different comics
+            #finalCharactersResultsList = [dict(t) for t in {tuple(d.items()) for d in finalCharactersResultsList}]
+
+
+    # given characterName, query information from API
+    def fetch_characters_from_target_comic(self, comicId, finalCharactersResultsList):
+
+        # extract characters from provided comic
+        rawCharactersFromTargetComic = self.fetch_content_from_api(queryURI="comics/{}/characters?".format(comicId))['data']['results']
+
+        # traverse through all characters
+        for character in rawCharactersFromTargetComic:
+
+            # add character if not yet encountered before
+            if character["id"] not in finalCharactersResultsList.keys():
+
+                # append character from comic
+                finalCharactersResultsList[character["id"]] = (
+                    character['id'],
+                    character['name'],
+                    self.fetch_comics_from_character_record(character['comics']['items']),
+                    character['thumbnail']['path'] + "." + character['thumbnail']['extension'],
+                    character['description']
+                )
+            
+
+        return finalCharactersResultsList
+
+
+    # given comics list, return back all strings, make sure they're void of bad characters
+    def fetch_comics_from_character_record(self, comics):
+        comicNames = ""
+        for comic in comics:
+            comicName = comic['name']
+            comicNames += "{}, ".format(comicName)
+        return comicNames
